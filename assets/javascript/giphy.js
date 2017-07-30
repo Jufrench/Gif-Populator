037f62e1727d9a61f6ba725c4de1759f69343b6d
Julian French <ju.french@gmail.com>
2017-07-29 21:49:41 -0500

    got api key

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
 function buttonMaker() {
   for (var i = 0; i < topics.length; i++) {
     $(".buttons").append("<button>" + topics[i] + "</button>");
-}
+  }
 }
 buttonMaker();
 
@@ -17,6 +17,10 @@ buttonMaker();
 // Make sure to use .trim() to get rid of the spaces on the sides
 
 // Call Giphy API
+// Api Key: c4474e5f5426451f818bdcfeeb43beb8
+$.ajax("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=c4474e5f5426451f818bdcfeeb43beb8&limit=5").done(function(data) {
+
+})
 
 // When button created is clicked, populate(append) 10 static gifs in html
 // Each time a button is clicked, a gif for that theme should be appended onto the DOM
